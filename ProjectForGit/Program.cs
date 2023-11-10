2ece6f4857e006bd68c91e15bac0023c5baf0629
okshurochka <okshurochka@yandex.ru>
2023-11-10 17:34:26 +0300

    Удалены лишние комментарии.

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -3,13 +3,8 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            //Подзадача 1
-            //Подзадача 1.1
+        {            
             Console.WriteLine("Hello, World!");
-            //Подзадача 2
-            //Подзадача 3
-            //Подзадача 4
         }
     }
 }
